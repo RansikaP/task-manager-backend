5b59e1e41832367833b5e3dd77b7ba083aadd664
Ransika <57299043+RansikaP@users.noreply.github.com>
2024-04-02 19:46:48 -0400

    hashing passwords
    adding hashing for passwords when creating new user
    used bcrypt

--- schema/user.js
+++ schema/user.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt')
 const { Schema, model } = mongoose;
 
 
@@ -19,6 +20,17 @@ const UserSchema = new Schema({
 },
     { collection: 'user-data' }
 );
+
+UserSchema.pre('save', async function (next) {
+    try {
+        const salt = await bcrypt.genSalt(10)
+        const hashedPassword = await bcrypt.hash(this.password, salt)
+        this.password = hashedPassword
+        next()
+    } catch (error) {
+        next(error)
+    }
+})
 
 const UserModel = model('User', UserSchema);
 
